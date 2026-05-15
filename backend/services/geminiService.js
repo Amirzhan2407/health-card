5f0a7b3d5531245ecb27b98fca81b82886e9970e
Amirzhan2407 <yechshanovamirzhan@gmail.com>
2026-05-15 19:06:14 +0500

    update

--- backend/services/geminiService.js
+++ backend/services/geminiService.js
@@ -13,8 +13,7 @@ export async function askGemini(message) {
 ${message}
 `;
 
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
-
+ const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
   const response = await axios.post(url, {
     contents: [
       {
