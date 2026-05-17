867bf069f778bd8fd22fe423fdbbc8d5fedbf3e8
Amirzhan2407 <yechshanovamirzhan@gmail.com>
2026-05-17 12:35:05 +0500

    update

--- backend/services/geminiService.js
+++ backend/services/geminiService.js
@@ -26,8 +26,7 @@ export async function askGemini(message) {
 Вопрос пользователя:
 ${message}
 `;
-
- const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
   const response = await axios.post(url, {
     contents: [
       {
