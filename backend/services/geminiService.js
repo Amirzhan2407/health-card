804bb36257fb1266370f3f34b42d5a264e174e21
Amirzhan2407 <yechshanovamirzhan@gmail.com>
2026-05-17 21:39:39 +0500

    change openrouter model

--- backend/services/geminiService.js
+++ backend/services/geminiService.js
@@ -10,7 +10,7 @@ export async function askGemini(message) {
     const response = await axios.post(
       "https://openrouter.ai/api/v1/chat/completions",
       {
-        model: "mistralai/mistral-7b-instruct:free",
+        model: "openrouter/free",
 
         messages: [
           {
